f3d70b7ed6fddb9d26ab2c958a3f866004996cc9
Oluwaseyi Fadahunsi <youngfadd@gmail.com>
2026-02-07 11:35:08 -0500

    Update authentication URLs

--- user_creation/urls.py
+++ user_creation/urls.py
@@ -7,8 +7,8 @@ from . import views
 
 app_name = 'user-creation'
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup'),
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('reg-2kp9w5/', views.SignUpView.as_view(), name='signup'),
+    path('auth-7vm4x2/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
 
